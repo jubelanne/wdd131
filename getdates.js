1618f356412f385394e3cc4d36c6165aff0743ce
jubel <jubeldelacruz@gmail.com>
2025-01-11 10:16:49 -0500

    new message

---
+++ getdates.js
@@ -0,0 +1,5 @@
+// Get Current Year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get Last Modified Date
+document.getElementById("lastModified").textContent += ` ${document.lastModified}`;
